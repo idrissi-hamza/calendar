cee9bbc8b9a84684dffc51fbad4c053fa2b9ea2f
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-03-27 14:02:58 +0200

    display  month calendar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { getMonth } from "./util";
+import dayjs from "dayjs";
+import NoWorkResult from "postcss/lib/no-work-result";
+import { Fragment } from "react";
 function App() {
+  console.table(getMonth());
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid grid-cols-7 gap-2 m-3 ">
+      {getMonth()
+        .flat()
+        .map((el, i) => (
+          <div className="p-2 bg-gray-300">
+            {i < 7 ? <div>{el.format("ddd")} </div> : ""}
+            <p key={i}>{el.format("DD")}</p>
+          </div>
+        ))}
     </div>
   );
 }
